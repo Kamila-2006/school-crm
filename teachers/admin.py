538da507f0d4aaea6a3b06958bb453568f9033d7
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-01-15 23:45:14 +0500

    admin-panel created

--- teachers/admin.py
+++ teachers/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.admin import StackedInline
 
-# Register your models here.
+from .models import Teacher
+
+
+class TeacherInline(StackedInline):
+    model = Teacher
+    extra = 0
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'subject', 'phone_number', 'email', 'years_of_work')
+    list_filter = ('first_name', 'last_name', 'subject')
+    search_fields = ('first_name', 'last_name', 'subject')
